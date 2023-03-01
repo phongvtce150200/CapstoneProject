06ca57dc2b806290f914dbc6f85ee492daa79ca2
F:\Do An\SourceTree <haupcce150659@fpt.edu.vn>
2023-03-01 16:24:57 +0700

    Create prescription and prescription detail

---
+++ ClinicManageAPI/DTO/PrescriptionDtos/PrescriptionsDTO.cs
@@ -0,0 +1,16 @@
+﻿using BusinessObject.Entity;
+using ClinicManageAPI.DTO.AppointmentDtos;
+using ClinicManageAPI.DTO.PrescriptionDetailDtos;
+using System.Collections.Generic;
+
+namespace ClinicManageAPI.DTO.PrescriptionDtos
+{
+    public class PrescriptionsDTO
+    {
+        public int AppointmentId { get; set; }
+        
+        public AppointmentPrescriptionDTO Appointment { get; set; }
+        public ICollection<PrescriptionDetailDTO> PrescriptionDetails { get; set; }
+
+    }
+}
---
+++ ClinicManageAPI/Extentions/PrescriptionManageExtentions.cs
@@ -0,0 +1,31 @@
+﻿using BusinessObject.Entity;
+using System;
+
+namespace ClinicManageAPI.Extentions
+{
+    public static class PrescriptionManageExtentions
+    {
+        public static Prescription PostPrescription(this Prescription prescription, string user)
+        {
+            prescription.CreatedDate = DateTime.UtcNow;
+            prescription.IsDelete = false;
+            prescription.CreatedBy = user;
+            return prescription;
+        }
+
+        public static Prescription UpdatePrescription(this Prescription prescription, string user)
+        {
+            prescription.UpdatedBy = user;
+            prescription.UpdatedDate = DateTime.UtcNow;
+            return prescription;
+        }
+
+        public static Prescription DeletePrescription(this Prescription prescription, string user)
+        {
+            prescription.IsDelete = true;
+            prescription.DeletedDate = DateTime.UtcNow;
+            prescription.DeletedBy = user;
+            return prescription;
+        }
+    }
+}
