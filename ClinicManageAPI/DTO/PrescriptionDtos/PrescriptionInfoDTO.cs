a064b111ec27776853838a8107bc074d0ba6d217
F:\Do An\SourceTree <haupcce150659@fpt.edu.vn>
2023-03-01 19:31:38 +0700

    Customer Create prescription

---
+++ ClinicManageAPI/DTO/PrescriptionDtos/PrescriptionInfoDTO.cs
@@ -0,0 +1,15 @@
+﻿using BusinessObject.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace ClinicManageAPI.DTO.PrescriptionDtos
+{
+    public class PrescriptionInfoDTO
+    {
+        public int? IdDoctor { get; set; }
+        public int? IdPatient { get; set; }
+        public DateTime CreateDate { get; set; }
+        public ICollection<PrescriptionDetails> PrescriptionDetails { get; set; }
+
+    }
+}
