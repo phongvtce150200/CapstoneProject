d7456308247e22195be8530ecf1db0787c7ffc23
naman001 <namtran.su@gmail.com>
2023-03-08 12:48:24 +0700

    udpated models n DTO 08/03/23

---
+++ BusinessObject/Configuration/MRITechnicianConfig.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObject.Entity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.Configuration
+{
+    public class MRITechnicianConfig : IEntityTypeConfiguration<MRITechnician>
+    {
+        public void Configure(EntityTypeBuilder<MRITechnician> builder)
+        {
+            builder.ToTable("MRITechnician");
+        }
+    }
+}
